97839eb3cb270b19f4394d2832a09e56a4306ec2
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-01-16 15:35:10 +0300

    AuthenticationModule mock

--- Authentication/AuthenticationModule.cs
+++ Authentication/AuthenticationModule.cs
@@ -1,8 +1,16 @@
-﻿using Nancy;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Nancy;
+
 namespace cmas.backend.Authentication
 {
     public class AuthenticationModule : NancyModule
     {
+        private List<UserRole> AvailableRoles;
+        private List<UserModel> Users;
+
+
         public AuthenticationModule()
         {
             After.AddItemToEndOfPipeline((ctx) => ctx.Response
@@ -10,23 +18,136 @@ namespace cmas.backend.Authentication
                 .WithHeader("Access-Control-Allow-Methods", "POST,GET")
                 .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type"));
 
+
+            MockAvailableRoles();
+            MockUsers();
+
             Post("/api/authentication/login", args =>
             {
+                var login = this.Request.Form["login"].Value;
+                var password = this.Request.Form["password"].Value;
 
-                return this.Response.AsJson(new
-                {
-                    id = 1,
-                    username = "Andrey.Kuzmenko",
-                    firstName = "Andrey",
-                    lastName = "Kuzmenko",
-                    token = "fake-jwt-token"
-                });
+                var user = (from u in Users where u.Login == login && u.Password == password select u).SingleOrDefault();
 
+                if (user != null)
+                    return Response.AsJson(user);
 
+                return HttpStatusCode.Forbidden;
             });
+        }
+
+
+        private void MockAvailableRoles()
+        {
+            AvailableRoles = new List<UserRole>();
+
+            int idCounter = 0;
+
+
+            AvailableRoles.Add(new UserRole
+            {
+                Id = ++idCounter, //    1
+                Description = "Старший менеджер"
+            });
+
+
+            AvailableRoles.Add(new UserRole
+            {
+                Id = ++idCounter, //    2
+                Description = "Менеджер"
+            });
+
+
+            AvailableRoles.Add(new UserRole
+            {
+                Id = ++idCounter, //    3
+                Description = "Руководитель"
+            });
+
+            AvailableRoles.Add(new UserRole
+            {
+                Id = ++idCounter, //    4
+                Description = "Подрядчик"
+            });
+        }
+
+        private void MockUsers()
+        {
+            Users = new List<UserModel>();
+            int idCounter = 0;
+
+
+            // Старший менеджер
+            {
+                var seniorManager = new UserModel
+                {
+                    Id = ++idCounter,
+                    FirstName = "Senior",
+                    LastName = "Manager",
+                    Login = "SeniorManager",
+                    Password = "111"
+                };
+
+                seniorManager.UserRole.Add((from r in AvailableRoles where r.Id == 1 select r)
+                    .Single());
+
+                Users.Add(seniorManager);
+            }
+
+
+            // Менеджер
+            {
+                var generalManager = new UserModel
+                {
+                    Id = ++idCounter,
+                    FirstName = "",
+                    LastName = "Manager",
+                    Login = "GeneralManager",
+                    Password = "111"
+                };
+
+                generalManager.UserRole.Add((from r in AvailableRoles where r.Id == 2 select r)
+                    .Single());
+
+                Users.Add(generalManager);
+            }
+
+
+            // Руководитель
+            {
+                var сhief = new UserModel
+                {
+                    Id = ++idCounter,
+                    FirstName = "",
+                    LastName = "Сhief",
+                    Login = "Сhief",
+                    Password = "111"
+                };
+
+                сhief.UserRole.Add((from r in AvailableRoles where r.Id == 3 select r)
+                    .Single());
+
+                Users.Add(сhief);
+            }
+
+            // Подрядчик
+            {
+                var contractor = new UserModel
+                {
+                    Id = ++idCounter,
+                    FirstName = "",
+                    LastName = "Contractor",
+                    Login = "Contractor",
+                    Password = "111"
+                };
+
+                contractor.UserRole.Add((from r in AvailableRoles where r.Id == 4 select r)
+                    .Single());
+
+                Users.Add(contractor);
+            }
         }
     }
 }
-
 
 
---
+++ Authentication/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace cmas.backend.Authentication
+{
+    public class UserRole
+    {
+        public int Id;
+
+        public string Description;
+    }
+}
