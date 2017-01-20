2e7ef643cdf7fc681d3fed216c18af8713cd75f8
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-01-20 10:45:29 +0300

    +requestmodule

---
+++ Request/RequestModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Nancy;
+
+namespace cmas.backend.Request
+{
+    public class RequestModule: GeneralModule
+    {
+        public RequestModule()
+        {
+            Get("/api/request/list/", args =>
+            {
+                var requests = (from r in Requests select r);
+
+                return Response.AsJson(requests);
+            });
+        }
+    }
+}
