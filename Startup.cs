64e29d43cc1e3c1eca5bc6fc8ae4015eb8a6ade9
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-20 09:26:37 +0300

    minor changes

--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,25 @@ namespace cmas.backend
 {
     using Microsoft.AspNetCore.Builder;
     using Nancy.Owin;
+    using System;
 
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
         {
+            app.Use(async (context, next) =>
+            {
+                /*
+                Console.WriteLine(context.Request.Path);
+                context.Request.Path = "/contract";
+                */
+                await next.Invoke();
+                
+            });
+
             app.UseOwin(x => x.UseNancy());
+
+
         }
     }
 }
