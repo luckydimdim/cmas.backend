0f879d849b0ba2ee75eb65afc2143a632cb85dcd
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-20 15:54:22 +0300

    couchdb client added

--- HomeModule.cs
+++ HomeModule.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Logging;
+using MyCouch;
 using Nancy;
+using System.Threading.Tasks;
 
 namespace cmas.backend
 {
@@ -7,12 +9,42 @@ namespace cmas.backend
     {
         private ILogger _logger;
 
-        public HomeModule(ILoggerFactory loggerFactory)
+        public class Person
         {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public HomeModule(ILoggerFactory loggerFactory) 
+        {
+            
+
             _logger = loggerFactory.CreateLogger<HomeModule>();
             _logger.LogInformation("test log");
 
-            Get("/", _ => "Hello from cmas.backend component!");
+            Get("/",sayHello);
         }
+
+        private async Task<object> sayHello(dynamic ct)
+        {
+            string name;
+
+            using (var client = new MyCouchClient("http://cmas-backend:backend967@cm-ylng-msk-03:5984", "cmas"))
+            {
+
+                /* var put = await client.Entities.PutAsync(
+                     new Person { Id = "001", Name = "Daniel" });
+                */
+
+                var get = await client.Entities.GetAsync<Person>("001");
+                name = get.Content.Name;
+
+
+    
+            }
+
+            return "hello from home module "+ name;
+        }
+
     }
 }
