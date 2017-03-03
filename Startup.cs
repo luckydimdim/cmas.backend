fcb8418fd6e904906c776569d80f192ecdecbead
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-03 09:26:33 +0300

    log app lifetime events

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,8 @@ namespace cmas.backend
 {
     public class Startup
     {
+        private ILogger _logger;
+
         public Startup(IHostingEnvironment env)
         {
             env.ConfigureNLog("nlog.config");
@@ -30,7 +32,7 @@ namespace cmas.backend
             return container.Resolve<IServiceProvider>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
         {
             env.EnvironmentName = "Production";    // TODO: настроить конфигурацию и использование EnvironmentName.Production;
 
@@ -46,6 +48,40 @@ namespace cmas.backend
             {
                 options.Bootstrapper = new Bootstrapper(app.ApplicationServices);
             }));
+
+            _logger = loggerFactory.CreateLogger<Startup>();
+
+            applicationLifetime.ApplicationStarted.Register(ApplicationStarted);
+            applicationLifetime.ApplicationStopping.Register(ApplicationStopping);
+            applicationLifetime.ApplicationStopped.Register(ApplicationStopped);
+
+        }
+
+        /// <summary>
+        /// Triggered when the application host has fully started and is about to wait for a graceful shutdown.
+        /// </summary>
+        /// <param name="logger"></param>
+        protected void ApplicationStarted()
+        {
+            _logger.LogInformation("ApplicationStarted");
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown. Requests may still be in flight. Shutdown will block until this event completes.
+        /// </summary>
+        /// <param name="logger"></param>
+        protected void ApplicationStopping()
+        {
+            _logger.LogInformation("ApplicationStopping");
+        }
+
+        /// <summary>
+        /// riggered when the application host is performing a graceful shutdown. All requests should be complete at this point. Shutdown will block until this event completes.
+        /// </summary>
+        /// <param name="logger"></param>
+        protected void ApplicationStopped()
+        {
+            _logger.LogInformation("ApplicationStopped");
         }
     }
 }
